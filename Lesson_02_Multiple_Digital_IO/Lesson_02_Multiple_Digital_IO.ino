2c3498bfcaa5c042b51f8c7cd94940e108a5de2c
ariffinzulkifli <ariffinastute@live.com>
2023-06-11 21:05:19 +0800

    add lesson 2

---
+++ Lesson_02_Multiple_Digital_IO/Lesson_02_Multiple_Digital_IO.ino
@@ -0,0 +1,27 @@
+void setup() {
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(9, HIGH); delay(100);
+  digitalWrite(9, LOW); delay(100);
+
+  digitalWrite(10, HIGH); delay(100);
+  digitalWrite(10, LOW); delay(100);
+
+  digitalWrite(11, HIGH); delay(100);
+  digitalWrite(11, LOW); delay(100);
+
+  digitalWrite(12, HIGH); delay(100);
+  digitalWrite(12, LOW); delay(100);
+
+  digitalWrite(11, HIGH); delay(100);
+  digitalWrite(11, LOW); delay(100);
+
+  digitalWrite(10, HIGH); delay(100);
+  digitalWrite(10, LOW); delay(100);
+}
