2b4b75ed098f9fd8570a56177a3bc39992a86a89
ariffinzulkifli <ariffinastute@live.com>
2023-06-13 22:54:03 +0800

    update sketch

--- ESP32/Lesson_20_Blynk_Control/Lesson_20_Blynk_Control.ino
+++ ESP32/Lesson_20_Blynk_Control/Lesson_20_Blynk_Control.ino
@@ -8,34 +8,48 @@
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
 
+#define LED1    18
+#define LED2    19
+#define LED3    21
+#define LED4    22
+
 char auth[] = BLYNK_AUTH_TOKEN;
 char ssid[] = "YOUR_WIFI_SSID";
 char pass[] = "YOUR_WIFI_PASSWORD";
 
-
 BLYNK_WRITE(V0) {
   int value = param.asInt();
 
-  pinMode(18, OUTPUT);
-  digitalWrite(18, value);
+  pinMode(LED1, OUTPUT);
+  digitalWrite(LED1, value);
 }
-
 
 BLYNK_WRITE(V1) {
   int value = param.asInt();
 
-  pinMode(19, OUTPUT);
-  digitalWrite(19, value);
+  pinMode(LED2, OUTPUT);
+  digitalWrite(LED2, value);
 }
 
+BLYNK_WRITE(V2) {
+  int value = param.asInt();
+
+  pinMode(LED3, OUTPUT);
+  digitalWrite(LED3, value);
+}
+
+BLYNK_WRITE(V3) {
+  int value = param.asInt();
+
+  pinMode(LED4, OUTPUT);
+  digitalWrite(LED4, value);
+}
 
 void setup() {
   Serial.begin(9600);
-  while(!Serial);
 
   Blynk.begin(auth, ssid, pass);
 }
-
 
 void loop() {
   Blynk.run();
--- ESP32/Lesson_21_Blynk_Monitor/Lesson_21_Blynk_Monitor.ino
+++ ESP32/Lesson_21_Blynk_Monitor/Lesson_21_Blynk_Monitor.ino
@@ -13,7 +13,12 @@ char auth[] = BLYNK_AUTH_TOKEN;
 char ssid[] = "YOUR_WIFI_SSID";
 char pass[] = "YOUR_WIFI_PASSWORD";
 
-int pinDHT11 = 23;
+#define LED1    18
+#define LED2    19
+#define LED3    21
+#define LED4    22
+#define DHTPIN  23
+
 SimpleDHT11 dht11(pinDHT11);
 
 unsigned long lastMillis = 0;
@@ -21,22 +26,33 @@ unsigned long lastMillis = 0;
 BLYNK_WRITE(V0) {
   int value = param.asInt();
 
-  pinMode(18, OUTPUT);
-  digitalWrite(18, value);
+  pinMode(LED1, OUTPUT);
+  digitalWrite(LED1, value);
 }
-
 
 BLYNK_WRITE(V1) {
   int value = param.asInt();
 
-  pinMode(19, OUTPUT);
-  digitalWrite(19, value);
+  pinMode(LED2, OUTPUT);
+  digitalWrite(LED2, value);
 }
 
+BLYNK_WRITE(V2) {
+  int value = param.asInt();
+
+  pinMode(LED3, OUTPUT);
+  digitalWrite(LED3, value);
+}
+
+BLYNK_WRITE(V3) {
+  int value = param.asInt();
+
+  pinMode(LED4, OUTPUT);
+  digitalWrite(LED4, value);
+}
 
 void setup() {
   Serial.begin(9600);
-  while(!Serial);
 
   Blynk.begin(auth, ssid, pass);
 }
@@ -51,15 +67,17 @@ void loop() {
   int err = SimpleDHTErrSuccess;
   
   if ((err = dht11.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
+    Serial.print("#");
     return;
   }
   
-  Serial.print("Data OK: ");
-  Serial.print((int)temperature); Serial.print(" *C, "); 
-  Serial.print((int)humidity); Serial.println(" %RH");
+  Serial.println("Temperature: " + String(temperature) + " °C");
+  Serial.println("Humidity: " + String(humidity) + " %RH");
 
   if(millis() - lastMillis > 15000){
-    Blynk.virtualWrite(V2, temperature);
-    Blynk.virtualWrite(V3, humidity);
+    Serial.println("=> Send data to Blynk App.");
+
+    Blynk.virtualWrite(V4, temperature);
+    Blynk.virtualWrite(V5, humidity);
   }
 }
